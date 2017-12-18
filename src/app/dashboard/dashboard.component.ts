d16d8b096d522ad080a3cc930ea623e777d09685
Jef Willems <jwillems04@gmail.com>
2017-12-18 13:25:02 +0100

    added clickfunction in dropdown menu list

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {DishNavComponent} from "../dish-nav/dish-nav.component";
-import {Observable} from "rxjs/Observable";
-import * as firebase from "firebase";
-import {AngularFireDatabase} from "angularfire2/database";
-import {AngularFireAuth} from "angularfire2/auth";
-import { CategoryService} from '../service/chosencategory.service';
+import { DishNavComponent } from '../dish-nav/dish-nav.component';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { CategoryService } from '../service/chosencategory.service';
 import { Subscription } from 'rxjs/Subscription';
 import { EventBusService } from '../service/event-bus.service';
 
@@ -17,12 +17,12 @@ export class DashboardComponent implements OnInit {
   dishesObservable: Observable<any[]>;
   user: Observable<firebase.User>;
   userPromise: Promise<firebase.User>;
-  foodKeys: string[];
   food: string[];
+  recipeIndex: string[];
 
   category: any;
+  categoryIndex = '';
   subscription: Subscription;
-
 
 
   constructor(private categoryService: CategoryService, private db: AngularFireDatabase,
@@ -39,6 +39,7 @@ export class DashboardComponent implements OnInit {
         this.user = this.afAuth.authState;
 
         this.processData();
+        this.getCategoryIndex();
       }
       if (!(auth && auth.uid)) {
         this.dishesObservable = null;
@@ -49,16 +50,64 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  sendCategoryIndex(dish) {
+    console.log(dish);
+
+    // if (this.dish = 'Appetizer') {
+    //   this.sendAppetizers();
+    // }
+    // if (document.getElementById('category').innerText = 'Soup') {
+    //   this.sendSoups();
+    // }
+    // if (document.getElementById('category').innerText = 'MainDish') {
+    //   this.sendMainDishes();
+    // }
+    // if (document.getElementById('category').innerText = 'Cake') {
+    //   this.sendCakes();
+    // }
+    // if (document.getElementById('category').innerText = 'Dessert') {
+    //   this.sendDesserts();
+    // }
+  }
+
+  private sendAppetizers() {
+    this.event.emit('changedCategoryIndex', {listPath: '/Appetizer'});
+  }
+  private sendSoups() {
+    this.event.emit('changedCategoryIndex', {listPath: '/Soup'});
+  }
+  private sendMainDishes() {
+    this.event.emit('changedCategoryIndex', {listPath: '/MainDish'});
+  }
+  private sendCakes() {
+    this.event.emit('changedCategoryIndex', {listPath: '/Cake'});
+  }
+  private sendDesserts() {
+    this.event.emit('changedCategoryIndex', {listPath: '/Dessert'});
+  }
+
+
   private processData(): void {
     this.event.observe('changedCategory').subscribe((value) => {
-      console.log(value.listPath);
+      // console.log(value.listPath);
       this.db.object(value.listPath).valueChanges().subscribe(values => {
         console.log(values);
         this.food = Object.keys(values);
-        console.log(Object.keys(values));
+        this.food.sort(function (a, b) {
+          return (values[a].__meta.order > values[b].__meta.order) ? 1 : ((values[a].__meta.order > values[b].__meta.order) ? -1 : 0);
+        });
+
+        // console.log(Object.keys(values));
       });
     });
   }
 
-
+  private getCategoryIndex(): void {
+    this.event.observe('changedCategoryIndex').subscribe((value) => {
+        this.db.object('/Dishes' + value.listPath + '/recipes').valueChanges().subscribe((recipes) => {
+          this.recipeIndex = Object.keys(recipes);
+          console.log(this.recipeIndex);
+        });
+    });
+  }
 }
